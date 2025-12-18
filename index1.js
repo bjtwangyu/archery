d3843813df7dc5005b3c3a0ca44dfad1ab01c363
MongoCat <18332955936@163.com>
2025-12-18 17:35:20 +0800

    WEB

---
+++ index1.js
@@ -0,0 +1,7 @@
+var logo = document.getElementsByClassName("img2")[0];
+logo.onmouseover = function(){
+	logo.className="img2 dis";
+}
+logo.onmouseout = function(){
+	logo.className="img2"					 
+}
